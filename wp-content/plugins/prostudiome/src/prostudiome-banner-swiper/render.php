a15c82999f2e109bc861a6d8bde1e76b33043db0
ProStudio.me <info@prostudio.me>
2025-05-20 07:06:45 +0000

    May 20, 2025, 9:06 AM

--- wp-content/plugins/prostudiome/src/prostudiome-banner-swiper/render.php
+++ wp-content/plugins/prostudiome/src/prostudiome-banner-swiper/render.php
@@ -10,8 +10,11 @@
 // Get banner posts from the banner-front-page category
 $category = get_term_by('slug', 'banner-front-page', 'category');
 if (!$category) {
+    error_log('Banner Swiper: Category "banner-front-page" not found');
     return;
 }
+
+error_log('Banner Swiper: Found category with ID ' . $category->term_id);
 
 $banners = get_posts([
     'post_type' => 'post',
@@ -20,8 +23,11 @@ $banners = get_posts([
 ]);
 
 if (empty($banners)) {
+    error_log('Banner Swiper: No posts found in category ' . $category->term_id);
     return;
 }
+
+error_log('Banner Swiper: Found ' . count($banners) . ' posts');
 
 // Get block options
 $options = [
@@ -44,16 +50,25 @@ $wrapper_attributes = get_block_wrapper_attributes([
 <div <?php echo $wrapper_attributes; ?>>
     <div class="swiper">
         <div class="swiper-wrapper">
-            <?php foreach ($banners as $banner) : 
+            <?php 
+            $valid_banners = 0;
+            foreach ($banners as $banner) : 
                 $main_heading = get_field('banner_-_main_heading', $banner->ID);
                 $main_image = get_field('banner_main_image', $banner->ID);
                 $subheading = get_field('banner_subheading', $banner->ID);
                 $text = get_field('banner_text', $banner->ID);
                 $link = get_field('banner_link', $banner->ID);
 
+                error_log('Banner Swiper: Processing post ' . $banner->ID);
+                error_log('Banner Swiper: Main heading: ' . ($main_heading ? 'yes' : 'no'));
+                error_log('Banner Swiper: Main image: ' . ($main_image ? 'yes' : 'no'));
+
                 if (!$main_image) {
+                    error_log('Banner Swiper: Skipping post ' . $banner->ID . ' - no main image');
                     continue;
                 }
+
+                $valid_banners++;
             ?>
             <div class="swiper-slide" style="background-image: url('<?php echo esc_url($main_image['url']); ?>');">
                 <div class="banner-content">
@@ -77,15 +92,18 @@ $wrapper_attributes = get_block_wrapper_attributes([
                     <?php endif; ?>
                 </div>
             </div>
-            <?php endforeach; ?>
+            <?php endforeach; 
+            
+            error_log('Banner Swiper: Total valid banners: ' . $valid_banners);
+            ?>
         </div>
 
-        <?php if ($options['navigation']) : ?>
+        <?php if ($options['navigation'] && $valid_banners > 1) : ?>
         <div class="swiper-button-prev"></div>
         <div class="swiper-button-next"></div>
         <?php endif; ?>
 
-        <?php if ($options['pagination']) : ?>
+        <?php if ($options['pagination'] && $valid_banners > 1) : ?>
         <div class="swiper-pagination"></div>
         <?php endif; ?>
     </div>
