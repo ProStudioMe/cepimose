d24a751ff9bdff0a68a92b0211e2b9c0d141a9ff
ProStudio.me <info@prostudio.me>
2025-06-02 07:33:06 +0000

    Jun 2, 2025, 9:33 AM

---
+++ wp-content/plugins/zoloblocks/build/extensions/AI/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-rich-text'), 'version' => '6f16f8cfd5d80361c072');
